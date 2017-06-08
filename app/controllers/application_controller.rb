b8b14b43e2597c1bb7791c4bc4411e9565d9bbc4
Ondrej Kubala <kub.ond@gmail.com>
2017-06-07 22:30:28 -0300

    Adds basic tests and basic controller actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
---
+++ spec/controllers/volunteers_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe VolunteersController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryGirl.create(:user)
+    sign_in @current_user
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    context "volunteer exists" do
+      it "returns http success" do
+        volunteer = FactoryGirl.create(:volunteer)
+        get :show, params: {id: volunteer.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "volunteer doesn't exist" do
+      it "redirects to volunteers index" do
+        volunteer_attributes = FactoryGirl.attributes_for(:volunteer) 
+        get :show, params: {id: 100}
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    before(:each) do
+      @vol_attributes = FactoryGirl.attributes_for(:volunteer, user: @current_user)
+      post :create, params: {volunteer: @vol_attributes}
+    end
+    context "valid volunteer" do
+      it "redirects to volunteer" do
+        expect(response).to have_http_status(:found)
+        #expect(response).to redirect_to("/volunteers/#{Volunteer.last.id}")
+      end
+    end
+  end
+
+  # describe "PATCH #update" do
+  #   it "returns http created" do
+  #     volunteer = FactoryGirl.create(:volunteer)
+  #     patch :update, params: {id: volunteer.id}
+  #     expect(response).to have_http_status(:created)
+  #   end
+  # end
+
+  # describe "DELETE #destroy" do
+  #   it "returns http deleted" do
+  #     volunteer = FactoryGirl.create(:volunteer)
+  #     delete :destroy, params: {id: volunteer.id}
+  #     expect(response).to have_http_status(:destroyed)
+  #   end
+  # end
+
+end
