5d33da66eb82b77542b9f6ec9210678946879f06
Ondrej Kubala <kub.ond@gmail.com>
2017-06-08 09:19:06 -0300

    Setup CanCanCan.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+
   private
 
   def record_not_found
