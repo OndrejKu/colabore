e6e65de42dceea90d817e18a1e79218c7773a225
Ondrej Kubala <kub.ond@gmail.com>
2017-06-12 07:40:03 -0300

    Create schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -99,10 +99,6 @@ ActiveRecord::Schema.define(version: 20170609021213) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "kind"
-    t.string   "image"
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
